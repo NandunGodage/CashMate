d04fdec116039eb31a0811d3e4312fe4ff1e0c6e
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-03-18 19:05:57 +0530

    add chart buttons

---
+++ app/src/main/java/com/example/cashmate/ui/category/PieChartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cashmate.ui.category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cashmate.R
+
+class PieChartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pie_chart)
+    }
+}
--- app/src/main/java/com/example/cashmate/ui/category/SlideshowViewModel.kt
+++ app/src/main/java/com/example/cashmate/ui/category/SlideshowViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.cashmate.ui.slideshow
+package com.example.cashmate.ui.category
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value =""
     }
     val text: LiveData<String> = _text
 }
