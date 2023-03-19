11e9fcd5772446bcf7bc837c07c6e5e997bc9552
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-03-20 01:23:24 +0530

    Create chart acivity

--- app/src/main/java/com/example/cashmate/ui/category/SlideshowFragment.kt
+++ app/src/main/java/com/example/cashmate/ui/category/SlideshowFragment.kt
@@ -1,12 +1,17 @@
 package com.example.cashmate.ui.category
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.cashmate.BarChartActivity
+import com.example.cashmate.PieChartActivity
+import com.example.cashmate.R
 import com.example.cashmate.databinding.FragmentSlideshowBinding
 
 class SlideshowFragment : Fragment() {
