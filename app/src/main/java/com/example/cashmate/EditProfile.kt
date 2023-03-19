fc21a278e3f1268ea5a1cb715423560e003b052f
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-03-20 01:16:51 +0530

    edit profile done

--- app/src/main/java/com/example/cashmate/EditProfile.kt
+++ app/src/main/java/com/example/cashmate/EditProfile.kt
@@ -1,12 +1,11 @@
-package com.example.cashmate.ui.category
+package com.example.cashmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cashmate.R
 
-class BarChartActivity : AppCompatActivity() {
+class EditProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bar_chart)
+        setContentView(R.layout.activity_edit_profile)
     }
 }
