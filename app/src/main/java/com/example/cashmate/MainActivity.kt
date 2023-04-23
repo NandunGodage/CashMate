50ff75934325de734054d91b402b151360f339f7
Nandun Godage <donnandun@gmail.com>
2023-04-24 01:32:25 +0530

    activity_main.xml buttons linked to activity_exp_category.xml & activity_overview.xml

--- app/src/main/java/com/example/cashmate/MainActivity.kt
+++ app/src/main/java/com/example/cashmate/MainActivity.kt
@@ -10,14 +10,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonFnD = findViewById<ImageView>(R.id.btnFnD) //declare the button as val and type is ImageView
-        buttonFnD.setOnClickListener {
-            // write what happen when you click the button
+            //food n drink button
+            val buttonFnD = findViewById<ImageView>(R.id.btnFnD) //declare the button as val and type is ImageView
+            buttonFnD.setOnClickListener {
+                // write what happen when you click the button
 
-            intent = Intent(this, ExpCategory::class.java)  //made intent and linked to activity
+                intent = Intent(this, ExpCategory::class.java)  //made intent and linked to activity
 
-            startActivity(intent)
+                startActivity(intent)
+            }
 
+            //med button
+            val buttonMed = findViewById<ImageView>(R.id.btnMed)
+            buttonMed.setOnClickListener {
+                 intent = Intent( this, ExpCategory::class.java)
+                startActivity(intent)
+            }
+
+            //shopping button
+            val buttonShop = findViewById<ImageView>(R.id.btnShop)
+            buttonShop.setOnClickListener {
+                intent = Intent( this, ExpCategory::class.java)
+                startActivity(intent)
+            }
+
+            //transport button
+            val buttonTrans = findViewById<ImageView>(R.id.btnTrans)
+            buttonTrans.setOnClickListener {
+                intent = Intent( this, ExpCategory::class.java)
+                startActivity(intent)
+            }
+
+            //other button
+            val buttonOther = findViewById<ImageView>(R.id.btnOther)
+            buttonOther.setOnClickListener {
+                intent = Intent( this, ExpCategory::class.java)
+                startActivity(intent)
+            }
+
+            //add income
+            val buttonAddIncome = findViewById<ImageView>(R.id.addIncome)
+            buttonAddIncome.setOnClickListener {
+                intent = Intent( this,activity_overview::class.java)
+                startActivity(intent)
+            }
+
+
+            /* //add card
+            val buttonAddCard = findViewById<ImageView>(R.id.addCard)
+            buttonAddCard.setOnClickListener {
+                intent = Intent( this,"add the addcard activity here"::class.java)
+                startActivity(intent)
+            } */
         }
-    }
 }
