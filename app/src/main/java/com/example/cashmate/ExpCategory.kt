4cd98583c2653e3d2651061ea61340d63b36bde8
Nandun Godage <donnandun@gmail.com>
2023-04-23 21:57:07 +0530

    activity_main.xml buttons linked to activity_exp_category.xml

---
+++ app/src/main/java/com/example/cashmate/ExpCategory.kt
@@ -0,0 +1,16 @@
+package com.example.cashmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+
+class ExpCategory : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exp_category)
+    }
+}
+
--- app/src/main/java/com/example/cashmate/MainActivity.kt
+++ app/src/main/java/com/example/cashmate/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.cashmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonFnD = findViewById<ImageView>(R.id.btnFnD) //declare the button as val and type is ImageView
+        buttonFnD.setOnClickListener {
+            // write what happen when you click the button
+
+            intent = Intent(this, ExpCategory::class.java)  //made intent and linked to activity
+
+            startActivity(intent)
+
+        }
     }
 }
