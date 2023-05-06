d8c509edacd170cb22b238068f70ef07b52827bc
IndudiniThennakoon <thennakoonindudini@gmail.com>
2023-05-07 00:30:17 +0530

    Display User name and password in navbar

--- app/src/main/java/com/example/cashmate/Navigation.kt
+++ app/src/main/java/com/example/cashmate/Navigation.kt
@@ -2,6 +2,7 @@ package com.example.cashmate
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,9 +13,17 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cashmate.databinding.ActivityNavigationBinding
+import com.example.cashmate.models.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class Navigation : AppCompatActivity() {
+
+    var myUserNameVal: TextView? = null
+    var myemailVal: TextView? = null
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityNavigationBinding
@@ -41,6 +50,19 @@ class Navigation : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
+
+
+        myemailVal = findViewById(R.id.myEmail)
+        myUserNameVal = findViewById(R.id.myName)
+
+        //get username and email from in shared preferences
+        val sharedPref = getSharedPreferences("MyPref", MODE_PRIVATE)
+        val email = sharedPref?.getString("email", "")
+        val password = sharedPref?.getString("password", "")
+
+        readData(email!!, password!!)
+
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
@@ -54,5 +76,47 @@ class Navigation : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_test)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun readData(email: String, password: String) {
+        //get user details from database by using email and password
+        val dbRef = FirebaseDatabase.getInstance().getReference("Users")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (userSnapshot in snapshot.children) {
+                        val user = userSnapshot.getValue(User::class.java)
+                        println("user: ${user.toString()}}")
+                        if (user != null) {
+                            if (user.email == email && user.password == password) {
+                                //user found
+                                //save user details in shared preferences
+                                //navigate to home page
+
+                                //save user details in shared preferences
+                                val sharedPref =
+                                    getSharedPreferences(
+                                        "MyPref",
+                                        AppCompatActivity.MODE_PRIVATE
+                                    )
+                                val editor = sharedPref?.edit()
+                                editor?.putString("email", user.email)
+                                editor?.putString("password", user.password)
+                                editor?.putString("userName", user.userName)
+                                editor?.apply()
+                                //set username and email in edit text
+                                myemailVal?.setText(user.email)
+                                myUserNameVal?.setText(user.userName)
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("error: ${error.message}")
+            }
+        })
+
     }
 }
